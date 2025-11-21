6a1cef6da5df21d247e585fed46a9464e79991ae
leidZhang <113314347+leidZhang@users.noreply.github.com>
2025-11-21 15:29:35 -0500

    Update install_glog.sh
    Signed-off-by: leidZhang <113314347+leidZhang@users.noreply.github.com>

--- scripts/install_glog.sh
+++ scripts/install_glog.sh
@@ -2,7 +2,6 @@
 apt remove libgoogle-glog-dev
 apt remove libgoogle-glog0v5
 
-cd /workspace/deps
 wget https://github.com/google/glog/archive/refs/tags/v0.6.0.tar.gz
 tar -xvzf v0.6.0.tar.gz
 rm v0.6.0.tar.gz && cd glog-0.6.0
