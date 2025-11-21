5cb8fefeae6af4759d71833cbcf662c70629bede
leidZhang <113314347+leidZhang@users.noreply.github.com>
2025-11-21 15:44:32 -0500

    Update install_ceres.sh
    Signed-off-by: leidZhang <113314347+leidZhang@users.noreply.github.com>

--- scripts/install_ceres.sh
+++ scripts/install_ceres.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clone -b 1.14.x https://github.com/ceres-solver/ceres-solver.git
-cd ceres-solver && mkdir build && cd build
+cd cd "$PWD/ceres-solver" && mkdir build && cd build
 cmake ..
 make -j$(nproc)
 make install
